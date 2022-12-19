07f83867eb89ebb3114e4da8896de48137b4f201
sunil880018 <sunil880018@gmail.com>
2022-12-19 09:12:24 +0530

    adding errors Procfile

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ import { dbConnection } from "./database/db.js";
 import bodyParser from "body-parser";
 import { apiRequestLimiter } from "./middleware/apiRateLimiter.js";
 import { CONFIG } from "./config/config.js";
+import {
+  UnauthenticatedError,
+  NotFoundError,
+  BadRequestError,
+  UnauthorizedError,
+} from "./errors/index.js";
 
 dbConnection();
 const app = express();
@@ -12,13 +18,19 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(apiRequestLimiter);
 
-
-
-app.get("/",(req,res)=>{
-    res.send("Hello World");
+app.get("/", (req, res) => {
+  const unauthenticated = new UnauthenticatedError("unauthenticated error");
+  const notFoundError = new NotFoundError("not found error");
+  const badRequestError = new BadRequestError("badRequestError error");
+  const unauthorizedError = new UnauthorizedError("UnauthorizedError error");
+  const errorObj ={
+    unauthenticated,
+    notFoundError,
+    badRequestError,
+    unauthorizedError
+  };
+  res.send(errorObj);
 });
-
-
 
 app.listen(PORT, () => {
   console.log(`server run at ${PORT}`);
